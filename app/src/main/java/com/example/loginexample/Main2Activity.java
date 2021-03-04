852170df59a61d873e0fd1ef90d8b93d37a38b46
tunguyen97-dev <59884028+tunguyen97-dev@users.noreply.github.com>
2021-03-04 10:10:12 +0700

    Update Main2Activity.java
    commit trực tiếp trên git

--- app/src/main/java/com/example/loginexample/Main2Activity.java
+++ app/src/main/java/com/example/loginexample/Main2Activity.java
@@ -9,6 +9,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String name;
         setContentView(R.layout.activity_main2);
     }
 }
