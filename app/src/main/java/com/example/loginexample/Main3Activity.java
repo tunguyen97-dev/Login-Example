f9aee017e609f2f242ad11d17b45398dc146d417
Nguyễn Văn Tú <nguyenvantu@luvina.net>
2021-03-04 09:14:01 +0700

    thêm biến

--- app/src/main/java/com/example/loginexample/Main3Activity.java
+++ app/src/main/java/com/example/loginexample/Main3Activity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main3Activity extends AppCompatActivity {
-
+String nanme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
+
 }
